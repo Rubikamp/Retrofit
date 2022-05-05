5993181a55cf443f626cb252a62beae374750f81
Sana Ebadi <sanaebadi97@gmail.com>
2022-05-05 21:19:10 +0430

    called api and add some thing else like prpgrees bar

--- app/src/main/java/info/sanaebadi/retrofit/api/RetrofitClient.java
+++ app/src/main/java/info/sanaebadi/retrofit/api/RetrofitClient.java
@@ -1,5 +1,7 @@
 package info.sanaebadi.retrofit.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,9 +9,20 @@ public class RetrofitClient {
     private static RetrofitClient instance = null;
     private ApiInterfcae apiInterfcae;
 
+    public OkHttpClient getClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        return client;
+    }
+
     private RetrofitClient(){
         Retrofit retrofit = new Retrofit.Builder().baseUrl(ApiInterfcae.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(getClient())
                 .build();
         apiInterfcae = retrofit.create(ApiInterfcae.class);
     }
--- app/src/main/java/info/sanaebadi/retrofit/ui/MainActivity.java
+++ app/src/main/java/info/sanaebadi/retrofit/ui/MainActivity.java
@@ -1,28 +1,66 @@
 package info.sanaebadi.retrofit.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import java.util.List;
+
+import info.sanaebadi.retrofit.api.RetrofitClient;
 import info.sanaebadi.retrofit.databinding.ActivityMainBinding;
+import info.sanaebadi.retrofit.model.CityModelItem;
 import info.sanaebadi.retrofit.ui.adapter.CityNameAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private CityNameAdapter adapter;
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        configRecyclerView();
 
-        adapter= new CityNameAdapter();
-        binding.recyclerCityName.setHasFixedSize(true);
-        binding.recyclerCityName.setAdapter(adapter);
+        getCityNames();
+
 
     }
 
+    private void configRecyclerView() {
+        binding.recyclerCityName.setHasFixedSize(true);
+        binding.recyclerCityName.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+
+    }
+
+
+    private void getCityNames() {
+        Call<List<CityModelItem>> listCall = RetrofitClient.getInstance().getApiInterfcae().getCityNames();
+        listCall.enqueue(new Callback<List<CityModelItem>>() {
+            @Override
+            public void onResponse(Call<List<CityModelItem>> call, Response<List<CityModelItem>> response) {
+                binding.progressCircular.setVisibility(View.VISIBLE);
+                List<CityModelItem> cityModelItemList = response.body();
+                adapter = new CityNameAdapter(cityModelItemList);
+                binding.recyclerCityName.setAdapter(adapter);
+                binding.progressCircular.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onFailure(Call<List<CityModelItem>> call, Throwable t) {
+                binding.progressCircular.setVisibility(View.GONE);
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+    }
 
 }
